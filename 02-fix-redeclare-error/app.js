a77efd2cc6586d39112b77348de4fede168fd0b2
Awais Ahmad <awais6412l@gmail.com>
2022-12-01 18:12:58 +0500

    fix redeclare error

---
+++ 02-fix-redeclare-error/app.js
@@ -0,0 +1,3 @@
+// initialized and decalare a variable fullName of string as we use js
+var fullName = 'Awais Ahmad';
+console.log(fullName);
---
+++ 02-fix-redeclare-error/app.ts
@@ -0,0 +1,4 @@
+// initialized and decalare a variable fullName of string as we use js
+export {} // this we use to tell typescript this is different module from all others.
+const fullName:string = 'Awais Ahmad';
+console.log(fullName);
