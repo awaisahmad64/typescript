c11ed5d4f2b9216df78da105df84bc2b12295cbf
Awais Ahmad <awais6412l@gmail.com>
2023-01-10 12:07:42 +0500

    add array type

---
+++ 04-array/array.js
@@ -0,0 +1,4 @@
+var names = [];
+names.push('Awais');
+//names.push(3); this will make an error 'Argument of type 'number' is not assignable to parameter of type 'string'.'
+console.log(names[0]);
---
+++ 04-array/array.ts
@@ -0,0 +1,4 @@
+const names: string[] = [];
+names.push('Awais');
+//names.push(3); this will make an error 'Argument of type 'number' is not assignable to parameter of type 'string'.'
+console.log(names[0]);
