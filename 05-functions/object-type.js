65f2ea69b785a5ad8970a240867ee658865b6d6b
Awais Ahmad <awais6412l@gmail.com>
2023-01-12 07:31:23 +0500

    add optional value in car object

--- 05-functions/object-type.js
+++ 05-functions/object-type.js
@@ -1,6 +1,8 @@
+// add optional value mileage? : number
 var car = {
     type: 'Toyta',
     model: 'Corolla',
     year: 2008
 };
+car.mileage = 26400;
 console.log(car);
--- 05-functions/object-type.ts
+++ 05-functions/object-type.ts
@@ -1,6 +1,10 @@
-let car: { type: string; model: string; year: number } = {
+// add optional value mileage? : number
+// if we defined a type, we must declare, in optional, declare not matter
+let car: { type: string; model: string; year: number; mileage?: number } = {
   type: 'Toyta',
   model: 'Corolla',
   year: 2008,
 };
+car.mileage = 26400;
+
 console.log(car);
