023f8c0d50ca9e7dceb47886ce5929cc7a176b2a
Awais Ahmad <awais6412l@gmail.com>
2023-01-15 21:10:22 +0500

    create file with union type

---
+++ 05-functions/union.js
@@ -0,0 +1,5 @@
+function unionType(code) {
+    console.log("You code is ".concat(code));
+}
+unionType(404);
+unionType('404');
---
+++ 05-functions/union.ts
@@ -0,0 +1,5 @@
+function unionType(code: string | number) {
+  console.log(`You code is ${code}`);
+}
+unionType(404);
+unionType('404');
