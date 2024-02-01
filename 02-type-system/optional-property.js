46a17b25a44d20b2e528ee8165f3dbe6a9ece55b
Awais Ahmad <awais6412l@gmail.com>
2024-02-01 21:58:46 +0500

    added optional property

---
+++ 02-type-system/optional-property.js
@@ -0,0 +1,5 @@
+function printName(obj) {
+    console.log(" ".concat(obj.first, " and ").concat(obj.last));
+}
+printName({ first: 'Awais' });
+printName({ first: 'Awais', last: 'Ahmad' });
---
+++ 02-type-system/optional-property.ts
@@ -0,0 +1,5 @@
+function printName(obj: { first: string; last?: string }) {
+  console.log(` ${obj.first} and ${obj.last}`);
+}
+printName({ first: 'Awais' });
+printName({ first: 'Awais', last: 'Ahmad' });
