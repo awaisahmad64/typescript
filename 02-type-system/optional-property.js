10a5a315dd842a678df2d8b79b8c284489b9b506
Awais Ahmad <awais6412l@gmail.com>
2024-02-06 20:54:43 +0500

    added more property of object literal

--- 02-type-system/optional-property.js
+++ 02-type-system/optional-property.js
@@ -1,5 +1,6 @@
 function printName(obj) {
-    console.log(" ".concat(obj.first, " and ").concat(obj.last));
+    console.log(" ".concat(obj.first, " and ").concat(obj.last, " and ").concat(obj.id));
 }
 printName({ first: 'Awais' });
 printName({ first: 'Awais', last: 'Ahmad' });
+printName({ first: 'Tayyab', last: 'Sandhu', id: 1 });
--- 02-type-system/optional-property.ts
+++ 02-type-system/optional-property.ts
@@ -1,7 +1,6 @@
-function printName(obj: { first: string; last?: string }) {
-  console.log(` ${obj.first} and ${obj.last}`);
+function printName(obj: { first: string; last?: string; id?: number }) {
+  console.log(` ${obj.first} and ${obj.last} and ${obj.id}`);
 }
 printName({ first: 'Awais' });
 printName({ first: 'Awais', last: 'Ahmad' });
-
-
+printName({ first: 'Tayyab', last: 'Sandhu', id: 1 });
