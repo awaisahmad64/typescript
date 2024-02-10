586c36b3e815d07d1c5226ec6d356fc96f627010
Awais Ahmad <awais6412l@gmail.com>
2024-02-10 23:01:30 +0500

    added special type any

---
+++ 05-functions/typeAny.js
@@ -0,0 +1,5 @@
+function greet(name) {
+    console.log("".concat(name));
+}
+greet('Hi, Awais Ahmad');
+greet(123);
---
+++ 05-functions/typeAny.ts
@@ -0,0 +1,5 @@
+function greet(name: any) {
+  console.log(`${name}`);
+}
+greet('Hi, Awais Ahmad');
+greet(123);
